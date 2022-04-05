360ea8cda77287427fdc69b226a3b440f6c5104d
dhatuker <dhatukertayuga@gmail.com>
2022-04-04 22:56:33 -0700

    init git

--- zwallet/SceneDelegate.swift
+++ zwallet/SceneDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  SceneDelegate.swift
-//  zwallet
+//  zwallet-
 //
 //  Created by user217931 on 3/29/22.
 //
@@ -10,7 +10,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         self.setupAppRouter()
